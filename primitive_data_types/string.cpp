e4ab1d4effecef98de640f2cff5662f4d25ed4bf
Syed zain ali shah <zaingit780@gmail.com>
2024-11-14 10:39:50 +0500

    learning string

---
+++ primitive_data_types/string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string favorite = "My favorites fruit is \"leamon\" \n";
+    string name = "My name is Zain";
+    cout << favorite << name << endl;
+    return 0;
+}
