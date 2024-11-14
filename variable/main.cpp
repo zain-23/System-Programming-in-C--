931bbe6be597a315f875cb4cd66fdb8df7dc8ebc
Syed zain ali shah <zaingit780@gmail.com>
2024-11-14 06:56:39 +0500

    learn variable

---
+++ variable/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    const int balance = 10;
+    int addressNumber = 5;
+
+    cout << balance << endl;
+    cout << addressNumber <<endl;
+    cout << "Hello world" << endl;
+    return 0;
+}
