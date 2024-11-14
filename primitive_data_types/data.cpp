e9e8f580a5941a7f90d3448896feec9e280582f1
Syed zain ali shah <zaingit780@gmail.com>
2024-11-14 09:20:45 +0500

    learn primitive data types

---
+++ primitive_data_types/data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int teaLeaves = 50;
+    float waterTemperature = 89.9;
+    double priceOfTea = 299.99;
+    char teaGrade = 'A';
+    bool isReady = 0;
+
+    cout << isReady << endl;
+
+    return 0;
+}
