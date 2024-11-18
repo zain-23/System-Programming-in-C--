00c789758fd100f9f77d9a493bbd8440e6069bb9
Syed zain ali shah <zaingit780@gmail.com>
2024-11-19 00:38:55 +0500

    data types

---
+++ user_input/calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "ENTER A NUMBER: ";
+    cin >> x;
+    cout << "ENTER ANOTHER NUMBER: ";
+    cin >> y;
+    cout << "Sum is: " << x + y;
+    return 0;
+}
---
+++ user_input/cin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int integer;
+    cout << "Type a number :";
+    cin >> integer;
+    cout << "Your number is : " << integer;
+    return 0;
+}
