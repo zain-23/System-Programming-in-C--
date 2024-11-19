8800649a1d04f78505fadcb241bc6ce05be184aa
Syed zain ali shah <zaingit780@gmail.com>
2024-11-20 00:26:12 +0500

    learn strings in cpp

---
+++ string/string.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // string greeting = "Hello World";
+    // string Concatenation
+
+    // string firstName = "Syed Zain ";
+    // string lastName = "Ali Shah";
+    // string fullName = firstName.append(lastName);
+
+    // string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Find length
+
+    // Accessing string
+    // string txt = "JELLO";
+    // txt.at(0) = 'H';
+
+    // Special Character
+    /*
+    string txt = "We are the so-called \"Vikings\" from the north.";
+    string tx2 = "We\'re are the so-called \"Vikings\" from the north.";
+    */
+
+    /*
+
+     string fullName;
+     cout << "Enter your name: ";
+     getline(cin, fullName);
+    */
+
+    // C-Style string
+    string greeting1 = "Hello"; // Regular String
+    char greeting2[] = "Hello"; // Regular String
+    cout << "Your name is: " << greeting2;
+    return 0;
+}
