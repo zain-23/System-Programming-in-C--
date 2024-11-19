b73ebf530fcfa923af2e76355443a1e895bae92b
Syed zain ali shah <zaingit780@gmail.com>
2024-11-20 00:35:39 +0500

    learn math function in cpp with (cmath)

---
+++ math/cp_math.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    cout << sqrt(64) << endl;
+    cout << round(2.9) << endl;
+    cout << log(2) << endl;
+    return 0;
+}
---
+++ math/math_cpp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << max(10, 20) << "\n";
+    cout << min(10, 20);
+    return 0;
+}
